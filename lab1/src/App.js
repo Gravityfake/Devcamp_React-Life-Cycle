d1443c640918c8fb96a5abbbe11a0472b76269c8
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-26 10:59:54 +0700

    save

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -24,9 +24,24 @@ class Data extends React.Component {
     console.log('componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    return nextState.value < 10 ? true : false
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    return 'UPDATE'
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(snapshot);
+    console.log('componentDidUpdate');
+  }
+
   render() {
-    console.log(this.props);
-    return <h1>{this.props.name}</h1>
+    console.log('render');
+    return <><h1>{this.props.name}</h1><h3>{this.state.value}</h3><input type="button" value="click" onClick={() => this.setState({ value: this.state.value + 1 })} /></>
   }
 }
+
 
