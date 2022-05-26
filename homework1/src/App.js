46c2a80b61440dd92bd0ef311e056f0121b213c5
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-26 14:00:36 +0700

    save

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { name: 'Unknown User' }
+  }
+  componentDidMount() {
+    const name = prompt("Type ur name !")
+    this.setState({ name: name })
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.name && nextState.name !== " ") {
+      if(nextState.name === this.state.name) {
+        alert("คุณกรอกชื่อซ้ำ");
+      }
+      else{
+        let result = document.getElementById("result")
+        result.innerHTML += `<li style={{padding : "20px 20px"}} > prevName : ${this.state.name} <br/> newName : ${nextState.name} <br/> date : ${Date()} </li>`
+
+        return true
+      }
+    }
+    else {
+      this.setState({ name: "Unknown User" })
+      return false
+    }
+    // return nextState.name !== " " && nextState.name !== "" ? true : false
+  }
+
+  render() {
+    return (
+      <div style={{
+        display: "flex",
+        flexDirection: "column"
+      }}>
+        <h1>{this.state.name}</h1>
+        <input type={"text"}  style={{ width : "25%"}} id={"inp"}/>
+        <button onClick={()=>{
+          const value = document.getElementById("inp").value
+          this.setState({ name: value })
+        }} style={{ width : "25%"}} >Change Your Name</button>
+        <ul id='result'>
+        
+        </ul>
+      </div>
+
+    )
+  }
 }
-
-export default App;
