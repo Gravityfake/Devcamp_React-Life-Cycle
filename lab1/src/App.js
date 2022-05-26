984994b22ab69eb34771ff448c08f71bdd98efae
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-26 10:03:08 +0700

    save

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
-    console.log("Render");
     return (
-      <div>App</div>
-    )
+      <Data name="THIS IS TEST" />
+    );
   }
 }
+
+class Data extends React.Component {
+  constructor() {
+    super();
+    console.log('constructor');
+    this.state = { value: 1 }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+    return props
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  render() {
+    console.log(this.props);
+    return <h1>{this.props.name}</h1>
+  }
+}
+
